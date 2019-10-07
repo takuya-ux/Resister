14de4fa8a0ec35af33f47d137f2039f58d3c3fb2
takuya <1911140027r@ed.fuk.kindai.ac.jp>
2019-10-08 04:59:46 +0900

    購入画面の配置 出品画面の配置 を追加

---
+++ app/src/main/java/com/example/resister/enpty.java
@@ -0,0 +1,14 @@
+package com.example.resister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enpty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enpty);
+    }
+}
