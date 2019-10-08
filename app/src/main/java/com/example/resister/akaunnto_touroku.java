99730bb19a8ae27f4f2530b99794c96baf67d22d
takuya <1911140027r@ed.fuk.kindai.ac.jp>
2019-10-08 23:02:26 +0900

    アカウント登録画面の配置　を追加

---
+++ app/src/main/java/com/example/resister/akaunnto_touroku.java
@@ -0,0 +1,14 @@
+package com.example.resister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class akaunnto_touroku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akaunnto_touroku);
+    }
+}
